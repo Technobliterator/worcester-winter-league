ff2966e30b13960e5a959be2f3091a35cae1822a
Technobliterator <paulonis.miller@gmail.com>
2019-03-27 22:52:20 +0000

    test

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
     filename: 'build.js'
   },
   plugins: [
