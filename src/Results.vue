e3eb84ab9724bba4e4978fad6029564b209cf1ff
Technobliterator <paulonis.miller@gmail.com>
2019-03-28 22:19:59 +0000

    Updated based on latest results

--- src/Results.vue
+++ src/Results.vue
@@ -24,12 +24,12 @@
                         </tr>
                         <tr>
                             <td>Worcs A	208</td>
-                            <td>Halesowen	   144  adj 184</td>
+                            <td>Halesowen 144 adj 184</td>
                             <td>Redditch A	207 </td>
                         </tr>
                         <tr>
-                            <td>Pershore	184</td>
-                            <td>Droitwich 127  adj 167</td>
+                            <td>Pershore 184</td>
+                            <td>Droitwich 127 adj 167</td>
                             <td>Bromsgrove 170</td>
                         </tr>
                         <tr>
@@ -136,8 +136,8 @@
                     </tbody>
                 </table>
             </tab-pane>
-            <tab-pane label="Second round 2018">
-                Results of the second round:
+            <tab-pane label="Second round 2019">
+                Results of the second round galas on Saturday 16th March:
 
                 <table class="table">
                     <thead>
@@ -149,44 +149,250 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <th>Halesowen</th>
-                            <th>Droitwich</th>
-                            <th>Droitwich</th>
+                            <td>Worcester A 231</td>
+                            <td>Redditch A 222 adj 196</td>
+                            <td>Worcester C 171</td>
                         </tr>
                         <tr>
-                            <th>6pm for 6-30pm</th>
-                            <th>5-15pm for 5-35 start</th>
-                            <th>7-00pm for 7-20pm start</th>
+                            <td>Worcester B 172</td>
+                            <td>Pershore 190 adj 168</td>
+                            <td>Sandwell Sharks 164</td>
                         </tr>
+                        <tr>
+                            <td>Halesowen 157</td>
+                            <td>Droitwich 166 adj 147</td>
+                            <td>Sandwell Storm 162</td>
+                        </tr>
+                        <tr>
+                            <td>Orion 132</td>
+                            <td>Stourbridge 136 adj 120</td>
+                            <td>Redditch B 158</td>
+                        </tr>
+                        <tr>
+                            <td>Bromsgrove 93</td>
+                            <td>Perry Beeches 0</td>
+                            <td>Sandwell Strikers 139</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Club</th>
+                            <th>Gala Points</th>
+                            <th>Total Gala Points round 1 and 2</th>
+                            <th>League Points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                         <tr>
                             <td>Worcester A</td>
-                            <td>Redditch A</td>
-                            <td>Sandwell Sharks</td>
+                            <td>231</td>
+                            <td>439</td>
+                            <td>6+10+12 = 28</td>
                         </tr>
                         <tr>
-                            <td>Halesowen</td>
-                            <td>Pershore</td>
-                            <td>Sandwell Strikers</td>
-                        </tr>
-                        <tr>
-                            <td>Bromsgrove</td>
-                            <td>Droitwich</td>
-                            <td>Redditch B</td>
+                            <td>Redditch</td>
+                            <td>196</td>
+                            <td>403</td>
+                            <td>6+10+12 = 28</td>
                         </tr>
                         <tr>
                             <td>Worcester B</td>
-                            <td>Perry Beeches</td>
+                            <td>172</td>
+                            <td>335</td>
+                            <td>5+10+10 = 26</td>
+                        </tr>
+                        <tr>
+                            <td>Pershore</td>
+                            <td>168</td>
+                            <td>352</td>
+                            <td>5+10+11 = 26</td>
+                        </tr>
+                        <tr>
+                            <td>Halesowen</td>
+                            <td>157</td>
+                            <td>341</td>
+                            <td>4+10+12 = 26</td>
+                        </tr>
+                        <tr>
                             <td>Worcester C</td>
+                            <td>171</td>
+                            <td>290</td>
+                            <td>6+3 = 9</td>
+                        </tr>
+                        <tr>
+                            <td>Sandwell Sharks</td>
+                            <td>164</td>
+                            <td>303</td>
+                            <td>5+5 = 10</td>
+                        </tr>
+                        <tr>
+                            <td>Sandwell Storm</td>
+                            <td>162</td>
+                            <td>280</td>
+                            <td>4+2 = 6</td>
+                        </tr>
+                        <tr>
+                            <td>Redditch B</td>
+                            <td>158</td>
+                            <td>278</td>
+                            <td>3+2 = 5</td>
+                        </tr>
+                        <tr>
+                            <td>Droitwich</td>
+                            <td>147</td>
+                            <td>314</td>
+                            <td>4+11 = 15</td>
+                        </tr>
+                        <tr>
+                            <td>Sandwell Strikers</td>
+                            <td>139</td>
+                            <td>273</td>
+                            <td>2+2 = 4</td>
                         </tr>
                         <tr>
                             <td>Orion</td>
+                            <td>132</td>
+                            <td>288</td>
+                            <td>3+10 = 13</td>
+                        </tr>
+                        <tr>
+                            <td>Stourbridge</td>
+                            <td>120</td>
+                            <td>262</td>
+                            <td>3+9 = 12</td>
+                        </tr>
+                        <tr>
+                            <td>Bromsgrove</td>
+                            <td>93</td>
+                            <td>263</td>
+                            <td>1+11 = 12</td>
+                        </tr>
+                        <tr>
+                            <td>Perry Beeches</td>
+                            <td>0</td>
+                            <td>160</td>
+                            <td>0+10 = 10</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </tab-pane>
+            <tab-pane label="Next round">
+                The next round will be held on Sunday 14th April, all halas to be held at Droitwich Pool.
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Gala One</th>
+                            <th>Gala Two</th>
+                            <th>Gala Three</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>8:30am for 9:00am start</th>
+                            <th>11:00am for 11:30am start</th>
+                            <th>1:30pm for 2:00pm start</th>
+                        </tr>
+                        <tr>
+                            <td>Sandwell Strikers</td>
+                            <td>Worcester C</td>
+                            <td>Worcester A</td>
+                        </tr>
+                        <tr>
+                            <td>Orion</td>
+                            <td>Sandwell Sharks</td>
+                            <td>Redditch</td>
+                        </tr>
+                        <tr>
                             <td>Stourbridge</td>
                             <td>Sandwell Storm</td>
+                            <td>Worcester B</td>
+                        </tr>
+                        <tr>
+                            <td>Bromsgrove</td>
+                            <td>Redditch B</td>
+                            <td>Pershore</td>
+                        </tr>
+                        <tr>
+                            <td>Perry Beeches</td>
+                            <td>Droitwich</td>
+                            <td>Halesowen</td>
                         </tr>
                         <tr>
                             <th>Bromsgrove to organise</th>
                             <th>Droitwich to organise</th>
-                            <th>Sandwell to organise</th>
+                            <th>Pershore to organise</th>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Club</th>
+                            <th>Position</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Worcester A</td>
+                            <td>28</td>
+                        </tr>
+                        <tr>
+                            <td>Redditch A</td>
+                            <td>28</td>
+                        </tr>
+                        <tr>
+                            <td>Pershore</td>
+                            <td>26</td>
+                        </tr>
+                        <tr>
+                            <td>Halesowen</td>
+                            <td>26</td>
+                        </tr>
+                        <tr>
+                            <td>Worcester B</td>
+                            <td>25</td>
+                        </tr>
+                        <tr>
+                            <td>Droitwich</td>
+                            <td>15</td>
+                        </tr>
+                        <tr>
+                            <td>Orion</td>
+                            <td>13</td>
+                        </tr>
+                        <tr>
+                            <td>Bromsgrove</td>
+                            <td>12</td>
+                        </tr>
+                        <tr>
+                            <td>Stourbridge</td>
+                            <td>12</td>
+                        </tr>
+                        <tr>
+                            <td>Sandwell Sharks</td>
+                            <td>10</td>
+                        </tr>
+                        <tr>
+                            <td>Perry Beeches</td>
+                            <td>10</td>
+                        </tr>
+                        <tr>
+                            <td>Worcester C</td>
+                            <td>9</td>
+                        </tr>
+                        <tr>
+                            <td>Sandwell Storm</td>
+                            <td>6</td>
+                        </tr>
+                        <tr>
+                            <td>Redditch B</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>Sandwell Strikers</td>
+                            <td>4</td>
                         </tr>
                     </tbody>
                 </table>
